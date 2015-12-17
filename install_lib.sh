789739b5d6f39d5fdcc3d112e1fd0891fe023fcb
Kent Spark VM <texibtexib@gmail.com>
2015-12-17 07:05:56 +0000

    add lib install script

---
+++ install_lib.sh
@@ -0,0 +1,10 @@
+sudo apt-get install libxml2-dev libxslt1-dev python-dev
+sudo apt-get install python-lxml
+sudo pip install BeautifulSoup4
+sudo pip install jieba
+sudo pip install wordcloud
+sudo apt-get install python-imaging
+wget https://github.com/l10n-tw/cwtex-q-fonts-TTFs/raw/master/ttf/cwTeXQFangsong-Medium.ttf
+sudo pip uninstall numpy
+sudo apt-get remove python-numpy
+sudo pip install numpy==1.9
